4b8b83b2158b252d48f6c3e699a18089f41a8cad
SigurtO <sigurt.olsen@hotmail.com>
2026-04-17 14:38:58 +0200

    nothing really changed

--- Assets/Scripts/Cards/Effects/EffectPayload.cs
+++ Assets/Scripts/Cards/Effects/EffectPayload.cs
@@ -8,6 +8,12 @@ public struct EffectPayload
     [Tooltip("The logic to execute (e.g., DamageEffect, HealEffect)")]
     public CardEffect effect;
 
-    [Tooltip("The magnitude of the effect (e.g., 4 damage, 6 healing)")]
+    [Tooltip("The number of the effect (e.g., 4 damage, 6 healing)")]
     public int amount;
 }
+
+
+
+
+
+
